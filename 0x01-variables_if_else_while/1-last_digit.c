e02d81ca5c774dfa8e009cbfb036130bf24165b1
fanuelal <fanuelalmawstop.com>
2021-06-18 03:48:06 -0700

    Last digit evaluater

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,18 +14,18 @@ int n, L;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 L = n % 10;
-printf("the last digit of %d is %d\n", n, L);
+printf("the last digit of %d is %d", n, L);
 if (L > 5)
 {
-printf("%d is greater than 5", L);
+printf(" is greater than 5\n");
 }
 else if (L == 0)
 {
-printf("%d is 0", L);
+printf(" is 0\n");
 }
 else if (L < 6 && L != 0)
 {
-printf("%d is less than 6 and not 0", L);
+printf(" is less than 6 and not 0\n");
 }
 return (0);
 }
