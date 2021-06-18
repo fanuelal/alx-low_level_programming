e64f8d81854834e7c192d122b0475afc0e0f9a70
fanuelal <fanuelalmawstop.com>
2021-06-18 06:29:58 -0700

    print base 10  numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - prints the base 10 number
+ *
+ *Return: Always 0 success
+ **/
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
