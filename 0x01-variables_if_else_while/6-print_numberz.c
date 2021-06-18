03619f2e51fb0a4ed4380e8bec6be0c1e5ed7827
fanuelal <fanuelalmawstop.com>
2021-06-18 06:41:05 -0700

    print number by ascii

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - prints the alphabet in lowercase
+ *
+ *Return: Always 0 success
+ **/
+int main(void)
+{
+int ch;
+for (ch = 48; ch <= 58; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
