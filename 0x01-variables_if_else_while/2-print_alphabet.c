a9f2c415a5adfb45f1698c54baf48daf664385da
fanuelal <fanuelalmawstop.com>
2021-06-18 04:10:39 -0700

    lowercase letter display

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - prints the alphabet in lowercase
+ *
+ *Return: Always 0 success
+ **/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
