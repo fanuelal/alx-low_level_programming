951418a1654354d19802eccaddbca35c344c13b2
fanuelal <fanuelalmawstop.com>
2021-06-18 04:23:45 -0700

    print letters with out q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - prints the alphabet in lowercase
+ *
+ *Return: Always 0 success
+ **/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch == 'e' && ch == 'q')
+continue;
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
