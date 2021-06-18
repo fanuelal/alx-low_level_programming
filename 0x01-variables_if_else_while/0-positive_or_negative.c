b89a8fd6a0153c007f164d83fb0882dd0e1610e0
fanuelal <fanuelalmawstop.com>
2021-06-18 01:16:32 -0700

    Posetive and Negative int

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generats random numbers and identify posetive or nagative
+ *
+ * Return: Always 0 for success
+ *
+ **/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive \n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero", n);
+}
+else
+{
+printf("%d is negative", n);
+}
+return (0);
+}
