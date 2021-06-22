703b107dbe52dcd425b07c2739978c255a6de907
fanuelal <fanuelalmawstop.com>
2021-06-22 04:44:20 -0700

    dislay holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *main - dislay the holberton
+ *Return: 0 Always
+ **/
+int main(void)
+{
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+return (0);
+}
