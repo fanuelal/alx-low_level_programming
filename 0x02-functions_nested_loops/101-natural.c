13e46a0d95fff31ec4841ab0f6521f06ab25cd86
fanuelal <fanuelalmawstop.com>
2021-06-22 13:21:57 -0700

    add natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - print the sum of 1024
+ *Return: 0 Always
+ **/
+int main(void)
+{
+int n;
+for (int i = 1024; i >= 0; i--)
+{
+if ((i % 3 == 0) && (i % 5 == 0))
+{
+n += i;
+}
+}
+printf("%d\n", n);
+return (0);
+}
