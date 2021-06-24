b33784b7141f935b17a47841838fae2eace0c90e
fanuelal <fanuelalmawstop.com>
2021-06-24 10:24:14 -0700

    fixed

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,8 @@
 
 /**
 * main - causes an infinite loop
-* Return: 0
+* Return: 0 Always (success)
+*@\\o/: loop symbol
 */
 
 int main(void)
@@ -13,11 +14,10 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-/*while (i < 10)*/
-/*{*/
+/* while (i < 10) */
+/* { */
 putchar(i);
-/*  } */
-
+	/* } */
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
