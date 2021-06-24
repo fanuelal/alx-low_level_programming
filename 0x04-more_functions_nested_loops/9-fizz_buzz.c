e68a1beec51560f85eac66ab1481ee9ef285a605
fanuelal <fanuelalmawstop.com>
2021-06-24 09:12:22 -0700

    fizz and buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,11 +9,11 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if ((i % 3 != 0) && (i % 5 != 0))
+if (i % 3 != 0 && i % 5 != 0)
 {
 printf("%d", i);
 }
-else if ((i % 3 == 0) && (i % 5 == 0))
+else if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz");
 }
