2df92d4fad1081b8779744bda4aacecac42e1388
fanuelal <fanuelalmawstop.com>
2021-06-24 03:13:45 -0700

    print number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
 
 /**
- * print_numbers - multiplies two integers
- *
- * Return: the result of the operation
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
  */
 void print_numbers(void)
 {
-int index;
-for (i = 0; i <= 9; i++)
+int i;
+
+for (i = 0; i < 10; i++)
 {
-_putchar(i);
+_putchar(i + '0');
 }
+
 _putchar('\n');
 }
