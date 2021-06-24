1305b9a98cbf0311d521db0bf4b4c2893d33d316
fanuelal <fanuelalmawstop.com>
2021-06-24 00:24:51 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _isupper - check the uppercase of leetes.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+read me file about project 0x04-more_functions_nested_loops
