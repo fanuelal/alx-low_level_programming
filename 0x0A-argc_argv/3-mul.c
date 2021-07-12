26121d3166b18a4978cf0d035ad33981a4bee62f
fanuelal <fanuelalmawstop.com>
2021-07-12 12:46:21 -0700

    multiplication of arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+int i, d, n, len, f, digit;
+
+i = 0;
+d = 0;
+n = 0;
+len = 0;
+f = 0;
+digit = 0;
+while (s[len] != '\0')
+len++;
+
+while (i < len && f == 0)
+{
+if (s[i] == '-')
+++d;
+if (s[i] >= '0' && s[i] <= '9')
+{
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
+f = 0;
+}
+i++;
+}
+if (f == 0)
+return (0);
+return (n);
+}
+/**
+ * main - prints the each arguments in new line
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+int mult;
+if (argc == 3)
+{
+mult = _atoi(argv[1]) * _atoi(argv[2]);
+printf("%d\n", mult);
+}
+else
+{
+printf("Error\n");
+}
+return (0);
+}
