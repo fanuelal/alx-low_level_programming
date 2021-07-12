d01e73fdf72186d71af541878355520292707073
fanuelal <fanuelalmawstop.com>
2021-07-12 10:53:12 -0700

    numbers of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - prints the number of the arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc);
+return (0);
+}
