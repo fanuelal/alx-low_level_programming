0699872b68eb5e0dfb225787d3add2ed304619dd
fanuelal <fanuelalmawstop.com>
2021-07-19 08:57:02 -0700

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,6 @@
  *
  * Description: Longer description
  */
-dog_t *new_dog(char *name, float age, char *owner);
 struct dog
 {
 char *name;
@@ -18,4 +17,5 @@ char *owner;
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
