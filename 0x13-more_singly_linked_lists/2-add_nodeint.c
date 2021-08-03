541cfd2c8c570f2c7b3a47fe3699a64bd4be9b16
fanuelal <fanuelalmawstop.com>
2021-08-03 08:15:52 -0700

    add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,17 +1,16 @@
 #include "lists.h"
 /**
- * add_nodeint_end - adding node
+ * add_nodeint - adding node
  * @head: pointer pointing the listint_t
  * @n: the values of the structure
  * Return: return the new element address
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-int count = 0;
-listint_t *new = malloc(sizeof(listint_s));
+listint_t *new = malloc(sizeof(listint_t));
 if (!new)
 return (NULL);
-new->n = n
+new->n = n;
 new->next = *head;
 *head = new;
 return (*head);
