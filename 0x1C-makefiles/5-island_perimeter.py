2ebca9e8cd9dcaded086d3b5734dea142224a6df
fanuelal <fanuelalmawstop.com>
2021-11-24 12:24:56 -0800

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,12 +5,12 @@
 def island_perimeter(grid):
     """land zone parimeter calculater function"""
     Parim = 0
-    side=0
-    for i in range(0,len(grid)):
+    side = 0
+    for i in range(0, len(grid)):
         for j in range(0, len(grid[i])):
             if grid[i][j] == 1:
                 Parim += 4
-                for k in range(0,4):
+                for k in range(0, 4):
                     if k == 0:
                         if grid[i-1][j] == 1:
                             side += 1
@@ -23,7 +23,6 @@ def island_perimeter(grid):
                     elif k == 3:
                         if grid[i][j-1] == 1:
                             side += 1
-                
-                Parim= Parim - side
+                Parim = Parim - side
                 side = 0
     return Parim
