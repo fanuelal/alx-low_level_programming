d4de7525889a2995dbb2ffc22811876436df2dc4
fanuelal <fanuelalmawstop.com>
2021-11-24 12:46:36 -0800

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
-""" module that calculate the perimeter of island """
+"""Module that calculates the perimeter of an island in a grid."""
+
+
+def num_water_neighbors(grid, i, j):
+    """Returns the number of water neighbors a cell has in a grid."""
+
+    num = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        num += 1
+    if j <= 0 or not grid[i][j - 1]:
+        num += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        num += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        num += 1
+
+    return num
 
 
 def island_perimeter(grid):
-    """land zone parimeter calculater function"""
-    Parim = 0
-    side = 0
-    for i in range(0, len(grid)):
-        for j in range(0, len(grid[i])):
-            if grid[i][j] == 1:
-                Parim += 4
-                for k in range(0, 4):
-                    if k == 0:
-                        if grid[i-1][j] == 1:
-                            side += 1
-                    elif k == 1:
-                        if grid[i][j+1] == 1:
-                            side += 1
-                    elif k == 2:
-                        if grid[i+1][j] == 1:
-                            side += 1
-                    elif k == 3:
-                        if grid[i][j-1] == 1:
-                            side += 1
-                Parim = Parim - side
-                side = 0
-    return Parim
+    """Returns the perimeter of the island in grid."""
+
+    perim = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perim += num_water_neighbors(grid, i, j)
+
+    return perim
