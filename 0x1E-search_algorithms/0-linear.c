26a5841a51d198be96cb8f9be6041e84ef9246b8
fanuelal <fanuelalmawstop.com>
2022-01-19 08:13:06 -0800

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,21 +3,24 @@
 /**
  *linear_search - linear search
  *
+ *@array: search the value
+ *@size: size of arr
+ *@value: searched value
  *Return: the index of value searched
  **/
 
 int linear_search(int *array, size_t size, int value)
 {
-  size_t i = 0;
-  if (!array || size == 0)
-    return (-1);
-  while (i < size)
-    {
-      printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-	if (array[i] == value)
-	  return (i);
-      i++;
-    }
+size_t i = 0;
 
-  return (-1);
+if (!array || size == 0)
+return (-1);
+while (i < size)
+{
+printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return (i);
+i++;
+}
+return (-1);
 }
